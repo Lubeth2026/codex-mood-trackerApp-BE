e05547ce8f1d41c5b4533f507822d9f49909b891
Lubeth <lubethjimenez@yahoo.com>
2026-06-27 13:10:18 -0400

    Add a save route in app.py to retrieve form data saved from React, read it, add update, save it in Flask

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
 from flask_cors import CORS
+import json
 
 app = Flask(__name__)
 CORS(app, origins=["http://localhost:5173"])
@@ -11,3 +12,19 @@ def home():
         "message": "Hello from Flask. Welcome to the Mood Tracker!"
     })
 
+@app.route("/save", methods=["POST"])
+def save():
+    # GET the json sent from React
+    data = request.get_json()
+    # Open the json file and READ contents
+    with open("updates.json", "r") as file:
+        moods = json.load(file)
+    # ADD new mood to the list
+    moods.append(data)
+    # Save the updated list back into the json file
+    with open("updates.json", "w") as file:
+        json.dump(moods, file, indent=4)
+    # Send a response back to React    
+    return jsonify({
+        "message": "Mood Saved Successfully!"
+    })
