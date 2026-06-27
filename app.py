658e39f619274c2931603ba9269c589c2007366a
Lubeth <lubethjimenez@yahoo.com>
2026-06-26 20:56:48 -0400

    Add a home route in app.py to retreive the Welcome Message

--- app.py
+++ app.py
@@ -1,2 +1,13 @@
 
-print("Hello")
+from flask import Flask, jsonify
+from flask_cors import CORS
+
+app = Flask(__name__)
+CORS(app, origins=["http://localhost:5173"])
+
+@app.route("/")
+def home():
+    return jsonify({
+        "message": "Hello from Flask. Welcome to the Mood Tracker!"
+    })
+
